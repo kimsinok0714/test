897deaf3a7a4649a2dc0d5ec63622ed9638da335
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-14 06:52:35 +0900

    Refactor loginSlice registration in store.js

--- src/store.js
+++ src/store.js
@@ -6,7 +6,8 @@ import loginSlice  from './slices/loginSlice'
 export default configureStore({
 
     reducer: {
-        'loginSlice': loginSlice   // loginSlice 슬리이스로 만든 리듀서 함수
+		// loginSlice 슬리이스로 만든 리듀서 함수 등록
+        'login': loginSlice   // 슬라이스 이름: createSlice 함수를 사용하여 생성한 슬라이스
     }
     
 	// loginSlice 리듀서를 Redux Store에 등록하면 다음과 같은 형식으로 슬라이스의 State(상태)를 관리한다.
