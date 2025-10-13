4634ea3cfc41785a52924fc68ea9d32374f2fade
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-13 09:02:36 +0900

    Update store.js

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import loginSlice  from './slices/loginSlice'
 export default configureStore({
 
     reducer: {
-        'loginSlice': loginSlice
+        'loginSlice': loginSlice   // loginSlice 슬리이스로 만든 리듀서 함수
     }
     
 	// loginSlice 리듀서를 Redux Store에 등록하면 다음과 같은 형식으로 슬라이스의 State(상태)를 관리한다.
