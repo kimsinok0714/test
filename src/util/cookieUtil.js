a33ec1aa5228d14ce610462b240921b8d4b8783e
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-23 07:45:38 +0900

    Update cookieUtil.js

--- src/util/cookieUtil.js
+++ src/util/cookieUtil.js
@@ -3,17 +3,15 @@ import { Cookies } from "react-cookie";
 
 
 const cookies = new Cookies();
-
-
 // 쿠키 생성
 export const setCookies = ( name, value, days ) => {
 
     // cookie expire
     const expires = new Date();
-    expires.setUTCDate(expires.getUTCDate() + days);
+    expires.setUTCDate(expires.getUTCDate() + days);  // 객체의 UTC 기준 날짜(day) 부분만 가져옵니다.
 
-    // 쿠키 경로 설정 : 웹 사이트 모든 페이지에 쿠키를 사용할 수 있다. (보안상 주의)
-    cookies.set(name, value, { expires: expires, path: '/' }); 
+    // 쿠키 경로 설정 : 모든 요청 경로에  쿠키를 적용한다.
+    cookies.set(name, value, { expires: expires, path: '/' });   
 
 }
 
@@ -24,8 +22,6 @@ export const getCookies = ( name ) => {
 
 
 // 해당 경로에서 쿠키 삭제
-export const removeCookie = ( name, path='/' ) => {
-    
+export const removeCookie = ( name, path='/' ) => {    
     cookies.remove(name, {path: path});
-
 }
